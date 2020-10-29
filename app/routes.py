69e5e295bb9b9fe0b405bdaa460246538a9008d4
Maximilian Walther Gotthardt <walther@c54.math.tu-berlin.de>
2020-10-29 14:12:42 +0100

    Upload function enabled, opening the uploaded file as default

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,16 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
+# for uploading feature required
+import os
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 # 16 MB
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -26,3 +36,35 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect(url_for('/index'))
     return render_template('login.html', title='Sign In', form=form)
+
+# @app.route('/upload')
+# def upload():
+#     return render_template('upload.html', title='Upload')
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html', title='Upload')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    # run margot here?!
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
